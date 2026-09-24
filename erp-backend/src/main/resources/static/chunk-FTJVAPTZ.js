0e3c3c6f85b7dc31b39a4d005bec740e8cfdc1a2
TAMEN AYIBODO Albin Uriel <albinayibo45@gmail.com>
2026-09-24 17:43:00 +0100

    Appliquer les styles vente achat et commandes

---
+++ erp-backend/src/main/resources/static/chunk-FTJVAPTZ.js
@@ -0,0 +1,1 @@
+function o(r,m=!0){let n=r??0,t=new Intl.NumberFormat("fr-FR",{minimumFractionDigits:0,maximumFractionDigits:0}).format(n).replace(/[  ]/g," ");return m?`${t} FCFA`:t}export{o as a};
